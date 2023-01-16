1d8c307025110bd934f4f932cc186659613bb06a
fahimagithub <fahima96khan@gmail.com>
2023-01-16 14:49:12 +1100

    Complete testing

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import {  CategoryModel } from './db.js'
+import entryRoutes from './routes/entry_routes.js'
+import cors from 'cors'
+
+// const categories = ['Food', 'Work', 'Coding', 'Other']
+
+// const entries= [
+//     { category : 'Food', content: 'Hello'},
+//     { category : 'Coding', content: 'Express is cool!'},
+//     { category : 'Work', content: 'Another day at office'},
+// ]
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/', (request, response) => response.send({info : 'Journal API 2023'}))
+
+app.get('/categories', async(req,res) => res.send( await CategoryModel.find()))
+
+app.use('/entries', entryRoutes)
+
+
+export default app
---
+++ db.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.set('strictQuery' , true)
+
+async function dbClose(){
+    await mongoose.connection.close();
+    console.log("Database disconnected!");
+}
+
+try {
+    const m = await mongoose.connect(process.env.ATLAS_DB_URL)
+    console.log(m.connection.readyState === 1 ? 'Mongoose connected!' : 'Mongoose failed to connect')
+}
+catch (err) {
+    console.log(err)
+}
+  
+
+
+const entrySchema = new mongoose.Schema({
+    category: {type: mongoose.ObjectId, ref : 'Category'},
+    content: {type: String, required: true}
+})
+
+
+
+// Create a Mangoose Model based on the schema
+const EntryModel = mongoose.model('Entry', entrySchema)
+
+
+//Categories
+const  categorySchema = new mongoose.Schema({
+    name: {type: String, required: true}
+
+})
+
+const CategoryModel = mongoose.model('Category', categorySchema)
+
+export { EntryModel, CategoryModel , dbClose }
